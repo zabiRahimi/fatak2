5cb9be1ea797b654529d8287c6465dcc6f138411
zabiRahimi <rahimi.z1360@gmail.com>
2019-03-11 12:25:40 +0330

    edit migrate pros

--- database/migrations/2019_03_11_113024_create_table-pros.php
+++ database/migrations/2019_03_11_113024_create_table-pros.php
@@ -15,7 +15,26 @@ class CreateTablePros extends Migration
     {
         Schema::create('Pros', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->string('name' , 70);
+            $table->text('dis');
+            $table->string('price' , 8);
+            $table->string('old_price' , 8)->nullable();
+            $table->string('gram' , 8);
+            $table->string('gram_post' , 8);
+            $table->string('pakat_price' , 8);
+            $table->string('made' , 60)->nullable();
+            $table->string('model' , 70)->nullable();
+            $table->string('dimension' ,23)->nullable();
+            $table->json('mavad')->nullable();
+            $table->date('date_m')->nullable();
+            $table->date('date_n')->nullable();
+            $table->text('term')->nullable();
+            $table->text('bake')->nullable();
+            $table->string('sposor' , 50)->nullable();
+            $table->string('views' , 8);
+            $table->string('seller' , 60);
+            $table->boolean('show');
+
         });
     }
 
