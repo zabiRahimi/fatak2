96236beee74e1943b35358420a3c048a7b406fc7
zabiRahimi <rahimi.z1360@gmail.com>
2019-03-28 11:52:53 +0430

    zave

--- public/js/city.js
+++ public/js/city.js
@@ -1,6 +1,20 @@
-// css file on main.css
+
 function show_city(ostanId){
-  $('.selectOstan').css('display' , 'none');
-  $('.cityOff').removeClass('cityShow');
-  $('.'+ostanId).addClass('cityShow');
+
+
+  $('#ajax_sabad_city option[value="aval"]').prop('selected', true);
+  $('.sabad_kh_sefareshi2_1').html(0);
+  $('.sabad_kh_pishtaz2_1').html(0);
+  $('.sabad_kh_end_price2').html(0);
+  $(".form-check-input"). prop("checked", false);
+  if(ostanId=="no"){
+    $('.selectOstan').css('display' , 'block');
+    $('.cityOff').removeClass('cityShow');
+  }
+  else{
+    $('.selectOstan').css('display' , 'none');
+    $('.cityOff').removeClass('cityShow');
+    $('.'+ostanId).addClass('cityShow');
+  }
+
 }
