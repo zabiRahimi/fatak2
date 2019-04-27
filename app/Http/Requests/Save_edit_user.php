2ded2df4a151cc02567f0b2b5a6e819c599931bc
zabiRahimi <rahimi.z1360@gmail.com>
2019-04-27 14:46:24 +0430

    'ggggg'

--- app/Http/Requests/Save_edit_user.php
+++ app/Http/Requests/Save_edit_user.php
@@ -17,7 +17,22 @@ class Save_edit_user extends FormRequest
     {
         return true;
     }
-
+    public function validationData() {
+       $parametr=$this->all();
+       $karbary=$parametr['karbary'];
+       $mobail=$parametr['mobail'];
+       $num_farsi=array('/(\x{06F0})/ui','/(\x{06F1})/ui','/(\x{06F2})/ui','/(\x{06F3})/ui','/(\x{06F4})/ui','/(\x{06F5})/ui','/(\x{06F6})/ui','/(\x{06F7})/ui','/(\x{06F8})/ui','/(\x{06F9})/ui');
+       $num_english=array(0,1,2,3,4,5,6,7,8,9);
+       if(!empty($mobail)and !is_numeric($mobail)){
+         Request::merge([
+           'mobail'=>preg_replace($num_farsi, $num_english, $mobail),
+         ]);
+       }
+       Request::merge([
+         'karbary'=>preg_replace($num_farsi, $num_english, $karbary),
+       ]);
+       return $this->all();
+   }
     /**
      * Get the validation rules that apply to the request.
      *
