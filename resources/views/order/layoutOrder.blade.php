95439586297c7001aa6df5c3bb42a7b8b0163202
zabiRahimi <rahimi.z1360@gmail.com>
2019-05-04 17:48:09 +0430

    'eeee'

---
+++ resources/views/order/layoutOrder.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html  dir="rtl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="_token" content="{{csrf_token()}}" />
+        <title>@yield('title')</title>
+        <!-- Fonts -->
+         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+         <link href="\css\app.css" rel="stylesheet" type="text/css">
+         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"/>
+         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
+         <!-- Add the slick-theme.css if you want default styling -->
+         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css">
+         <link href="\css\main.css" rel="stylesheet" type="text/css">
+         <link href="\css\order.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+      <img src="http://localhost:8000/img_site/210.gif" class="gif_loding" alt="loding" >
+        <div class="">
+        <div class="content">
+          <div class="" >
+            @yield('content')
+          </div>
+       </div>
+        <footer>
+
+        </footer>
+        </div>
+
+        <script type="text/javascript" src="\js\app.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.min.js"></script>
+         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
+         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+         <script type="text/javascript" src="\js\main.js"></script>
+         <script type="text/javascript" src="\js\city.js"></script>
+        <script type="text/javascript" src="\js\order.js"></script>
+        <script type="text/javascript">
+           $(document).pjax('.apjax' , '#dashPjax');
+           $(function () {  $('[data-toggle="tooltip"]').tooltip()});
+        </script>
+    </body>
+</html>
